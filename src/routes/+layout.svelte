60cda0f0066e91b5c45056fd1bbb8ee89562235d
opedrosiqueira <opedrosiqueira@gmail.com>
2025-04-30 19:29:44 -0400

    prettier --write .

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,22 +1,22 @@
 <script>
-	import Header from '$lib/components/Header.svelte';
-	import SideBar from '$lib/components/SideBar.svelte';
+  import Header from '$lib/components/Header.svelte';
+  import SideBar from '$lib/components/SideBar.svelte';
 
-	import 'bootstrap/dist/css/bootstrap.css';
-	import 'bootstrap-icons/font/bootstrap-icons.css';
-	import * as bootstrap from 'bootstrap';
+  import 'bootstrap/dist/css/bootstrap.css';
+  import 'bootstrap-icons/font/bootstrap-icons.css';
+  import * as bootstrap from 'bootstrap';
 
-	let { children } = $props();
+  let { children } = $props();
 </script>
 
 <Header />
 
 <div class="container-fluid py-5 mt-1 mb-4">
-	{@render children()}
+  {@render children()}
 </div>
 
 <footer class="navbar bg-body-tertiary fixed-bottom">
-	<div class="container-fluid">Made with SvelteKit, CapacitorJS, Drizzle, Sql.js and Bootstrap</div>
+  <div class="container-fluid">Made with SvelteKit, CapacitorJS, Drizzle, Sql.js and Bootstrap</div>
 </footer>
 
 <SideBar />
