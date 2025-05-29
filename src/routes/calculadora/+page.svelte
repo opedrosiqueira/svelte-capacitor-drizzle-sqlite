117281b61acb935eb0f663ab96364fcd74a47e10
opedrosiqueira <opedrosiqueira@gmail.com>
2025-05-29 10:07:43 -0400

    ainda não está pronto. ainda falta testar.

--- src/routes/calculadora/+page.svelte
+++ src/routes/calculadora/+page.svelte
@@ -1,6 +1,29 @@
+<script>
+  let display = $state('');
+  let number = '';
+  let result = $state(0);
+
+  function input(digit) {
+    number += digit;
+    display = number;
+  }
+
+  function plus() {
+    result += Number(number);
+    number = '';
+    display = result;
+  }
+
+  function equals() {
+    display = Number(number) + result;
+    result = 0;
+    number = '';
+  }
+</script>
+
 <div class="text-center mt-3">
   <div class="row mb-2">
-    <div class="col"><input class="form-control" readonly /></div>
+    <div class="col"><input class="form-control" readonly bind:value={display} /></div>
   </div>
   <div class="row row-cols-4 g-2">
     <div class="col"><button class="btn bg-primary-subtle w-100">CE</button></div>
@@ -8,24 +31,24 @@
     <div class="col"><button class="btn bg-primary-subtle w-100">±</button></div>
     <div class="col"><button class="btn bg-primary-subtle w-100">%</button></div>
 
-    <div class="col"><button class="btn btn-primary w-100">7</button></div>
-    <div class="col"><button class="btn btn-primary w-100">8</button></div>
-    <div class="col"><button class="btn btn-primary w-100">9</button></div>
+    <div class="col"><button class="btn btn-primary w-100" onclick={() => input('7')}>7</button></div>
+    <div class="col"><button class="btn btn-primary w-100" onclick={() => input('8')}>8</button></div>
+    <div class="col"><button class="btn btn-primary w-100" onclick={() => input('9')}>9</button></div>
     <div class="col"><button class="btn bg-primary-subtle w-100">/</button></div>
 
-    <div class="col"><button class="btn btn-primary w-100">4</button></div>
-    <div class="col"><button class="btn btn-primary w-100">5</button></div>
-    <div class="col"><button class="btn btn-primary w-100">6</button></div>
+    <div class="col"><button class="btn btn-primary w-100" onclick={() => input('4')}>4</button></div>
+    <div class="col"><button class="btn btn-primary w-100" onclick={() => input('5')}>5</button></div>
+    <div class="col"><button class="btn btn-primary w-100" onclick={() => input('6')}>6</button></div>
     <div class="col"><button class="btn bg-primary-subtle w-100">*</button></div>
 
-    <div class="col"><button class="btn btn-primary w-100">1</button></div>
-    <div class="col"><button class="btn btn-primary w-100">2</button></div>
-    <div class="col"><button class="btn btn-primary w-100">3</button></div>
+    <div class="col"><button class="btn btn-primary w-100" onclick={() => input('1')}>1</button></div>
+    <div class="col"><button class="btn btn-primary w-100" onclick={() => input('2')}>2</button></div>
+    <div class="col"><button class="btn btn-primary w-100" onclick={() => input('3')}>3</button></div>
     <div class="col"><button class="btn bg-primary-subtle w-100">-</button></div>
 
-    <div class="col"><button class="btn btn-primary w-100">0</button></div>
+    <div class="col"><button class="btn btn-primary w-100" onclick={() => input('0')}>0</button></div>
     <div class="col"><button class="btn bg-primary-subtle w-100">.</button></div>
-    <div class="col"><button class="btn bg-primary-subtle w-100">=</button></div>
-    <div class="col"><button class="btn bg-primary-subtle w-100">+</button></div>
+    <div class="col"><button class="btn bg-primary-subtle w-100" onclick={equals}>=</button></div>
+    <div class="col"><button class="btn bg-primary-subtle w-100" onclick={plus}>+</button></div>
   </div>
 </div>
